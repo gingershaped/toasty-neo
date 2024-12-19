2608821160acd7c6b3eeab99e1aa2ceb2ef73ef0
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-19 22:43:06 +0000

    Clip owned rooms list scrollbar

--- src/app/(main)/rooms/(list)/add/RoomAddForm.tsx
+++ src/app/(main)/rooms/(list)/add/RoomAddForm.tsx
@@ -25,37 +25,39 @@ export function RoomAddForm({ isModerator }: { isModerator: boolean }) {
                 <option value={Host.SO}>Stack Overflow</option>
             </select>
             <input type="search" className="form-control mb-2" placeholder="Filter rooms" value={searchQuery} onInput={(e) => setSearchQuery(e.currentTarget.value)} />
-            <div className="border rounded overflow-y-auto flex-grow-1" style={{ containerType: "size" }}>
-                {ownedRoomsError != null && <div className="text-danger text-center">Failed to fetch rooms</div>}
-                {loadingOwnedRooms && (
-                    <div className="w-100 text-center mt-3">
-                        <div className="spinner-border text-primary" role="status">
-                            <span className="visually-hidden">Loading...</span>
+            <div className="border rounded flex-grow-1">
+                <div className="rounded overflow-y-auto h-100" style={{ containerType: "size", clipPath: "content-box" }}>
+                    {ownedRoomsError != null && <div className="text-danger text-center">Failed to fetch rooms</div>}
+                    {loadingOwnedRooms && (
+                        <div className="w-100 text-center mt-3">
+                            <div className="spinner-border text-primary" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
                         </div>
-                    </div>
-                )}
-                {filteredRooms != undefined && <>
-                    {(filteredRooms.length == 0 && !isModerator) && <div className="w-100 text-center text-secondary-emphasis">no rooms</div>}
-                    <ul className="list-group list-group-flush rounded">
-                        {isModerator && <li className="list-group-item hstack">
-                            <input className="form-check-input my-0 me-2 align-self-center" type="radio" name="roomId" value="custom" required />
-                            <input className="form-control" type="number" name="customRoomId" placeholder="Enter a room ID" />
-                        </li>}
-                        {filteredRooms.map(({ id, name }) => (
-                            <li key={id} className="list-group-item hstack">
-                                <input
-                                    className="form-check-input me-2"
-                                    type="radio"
-                                    name="roomId"
-                                    value={id}
-                                    id={`room-${id}`}
-                                    required
-                                />
-                                <label htmlFor={`room-${id}`}>{name}</label>
-                            </li>
-                        ))}    
-                    </ul>
-                </>}
+                    )}
+                    {filteredRooms != undefined && <>
+                        {(filteredRooms.length == 0 && !isModerator) && <div className="w-100 text-center text-secondary-emphasis">no rooms</div>}
+                        <ul className="list-group list-group-flush rounded">
+                            {isModerator && <li className="list-group-item hstack">
+                                <input className="form-check-input my-0 me-2 align-self-center" type="radio" name="roomId" value="custom" required />
+                                <input className="form-control" type="number" name="customRoomId" placeholder="Enter a room ID" />
+                            </li>}
+                            {filteredRooms.map(({ id, name }) => (
+                                <li key={id} className="list-group-item hstack">
+                                    <input
+                                        className="form-check-input me-2"
+                                        type="radio"
+                                        name="roomId"
+                                        value={id}
+                                        id={`room-${id}`}
+                                        required
+                                    />
+                                    <label htmlFor={`room-${id}`}>{name}</label>
+                                </li>
+                            ))}    
+                        </ul>
+                    </>}
+                </div>
             </div>
             <div className="form-text">only rooms you are an owner of are listed</div>
         </div>
