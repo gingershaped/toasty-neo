e61672895e4bb09d506f68fcb2c4091649b11ede
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-19 22:23:59 +0000

    Add a footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -19,5 +19,21 @@ export default async function MainLayout({ children }: { children: ReactNode }) 
         <main className="container-lg mt-5">
             {children}
         </main>
+        <footer className="bg-body-tertiary border-top ps-4 p-2 mt-auto hstack text-muted">
+            <p className="mb-0 me-auto">
+                ◈ Built by <a href="https://codegolf.stackexchange.com/users/108218/">Ginger</a>
+                {" "}for the fine people of <a href="https://stackexchange.net">Stack Exchange</a>
+                <br />
+                Server and domain generously provided by
+                {" "}<a href="https://langdev.stackexchange.com/users/5/rydwolf-programs">rydwolf</a>
+            </p>
+            <p className="mb-0 align-self-end">
+                <a href="https://chat.stackexchange.com/rooms/148706/i-cant-believe-its-not-botter">chat</a>
+                <span>&nbsp;•&nbsp;</span>
+                <a href="https://github.com/gingershaped/toasty-neo">source</a>
+                <span>&nbsp;•&nbsp;</span>
+                <a href="https://stackapps.com/q/10422/91091">stackapps</a>
+            </p>
+        </footer>
     </>;
 }
