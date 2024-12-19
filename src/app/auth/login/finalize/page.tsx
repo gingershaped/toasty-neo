7247a35ad365799acdd1c893af29814222d6576a
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-19 21:14:02 +0000

    Use browser APIs instead of useSearchParams

--- src/app/auth/login/finalize/page.tsx
+++ src/app/auth/login/finalize/page.tsx
@@ -2,13 +2,12 @@
 
 import { useEffect } from "react";
 import { finalizeLogin } from "./actions";
-import { useSearchParams } from "next/navigation";
 
 export default function LoginFinalize() {
-    const params = useSearchParams();
     useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
         finalizeLogin(params.get("code"), params.get("state"));
-    }, [params]);
+    }, []);
     return <main className="mt-5 text-center">
         <div>
             <p className="fs-2">Completing login</p>
