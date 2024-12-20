c612f62e7d597733d98583d1515976324d36cbe9
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-20 14:24:01 +0000

    Make the page footer responsive

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -19,7 +19,7 @@ export default async function MainLayout({ children }: { children: ReactNode }) 
         <main className="container-lg mt-5 mb-3">
             {children}
         </main>
-        <footer className="bg-body-tertiary border-top ps-4 p-2 mt-auto hstack text-muted">
+        <footer className="bg-body-tertiary border-top ps-4 p-2 mt-auto text-muted d-flex flex-column flex-sm-row">
             <p className="mb-0 me-auto">
                 ◈ Built by <a href="https://codegolf.stackexchange.com/users/108218/">Ginger</a>
                 {" "}for the fine people of <a href="https://stackexchange.net">Stack Exchange</a>
@@ -27,7 +27,7 @@ export default async function MainLayout({ children }: { children: ReactNode }) 
                 Server and domain generously provided by
                 {" "}<a href="https://langdev.stackexchange.com/users/5/rydwolf-programs">rydwolf</a>
             </p>
-            <p className="mb-0 align-self-end">
+            <p className="mb-0 align-self-sm-end">
                 <a href="https://chat.stackexchange.com/rooms/148706/i-cant-believe-its-not-botter">chat</a>
                 <span>&nbsp;•&nbsp;</span>
                 <a href="https://github.com/gingershaped/toasty-neo">source</a>
