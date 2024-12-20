ad07d95e0eb19762e49661239907b91fba6aae18
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-20 15:47:55 +0000

    Show a placeholder if a user has no rooms

--- src/app/(main)/users/[id]/rooms/page.tsx
+++ src/app/(main)/users/[id]/rooms/page.tsx
@@ -1,13 +1,26 @@
 import { prisma } from "@/lib/globals";
 import { getUser, UserParams } from "../user";
 import { RoomList } from "@/app/(main)/rooms/_components/RoomList";
+import { readUserSession } from "@/lib/auth/session";
+import Link from "next/link";
 
 export default async function UserRooms({ params }: { params: UserParams }) {
+    const currentUser = await readUserSession();
     const targetUser = await getUser(params);
     const rooms = await prisma.room.findMany({
         where: { jobCreatorId: targetUser.networkId },
         include: { jobCreator: true },
         orderBy: { jobCreatedAt: "desc" },
     });
-    return <RoomList rooms={rooms} />;
+    if (rooms.length > 0) {
+        return <RoomList rooms={rooms} />;
+    } else if (targetUser.networkId == currentUser?.networkId) {
+        return <div className="text-muted text-center">
+            You have not added any rooms. <Link href="/rooms/add">Add a room</Link>
+        </div>;
+    } else {
+        return <div className="text-muted text-center">
+            This user has not added any rooms.
+        </div>;
+    }
 }
