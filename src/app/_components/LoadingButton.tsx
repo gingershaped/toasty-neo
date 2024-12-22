12e914370196c02419e7a2fb160a0af28e941e65
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-22 22:57:51 +0000

    That's what I get for not using vscode to make that change

--- src/app/_components/LoadingButton.tsx
+++ src/app/_components/LoadingButton.tsx
@@ -54,7 +54,7 @@ export function LoadingLink({ href, variant, className, children }: LoadingLinkP
             className={`position-relative btn btn-${variant} ${className ?? ""} ${loading ? "disabled" : ""}`}
             role="button"
             onClick={() => setLoading(true)}
-            preload={false}
+            prefetch={false}
         >
             <LoadingContent loading={loading}>{children}</LoadingContent>
         </Link>
