cecdf65d2dc87c6ab10b0869a4499b03c91dc908
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-01-13 19:53:48 +0000

    Enable responsive navigation

--- src/app/(main)/Header.tsx
+++ src/app/(main)/Header.tsx
@@ -13,28 +13,33 @@ export type HeaderProps = {
 
 export default function Header({ user, children: routeNames }: HeaderProps) {
     const segment = useSelectedLayoutSegment() ?? "/";
-    return <nav className="navbar navbar-expand bg-body-tertiary">
-        <div className="container-lg">
+    return <nav className="navbar navbar-expand-md bg-body-tertiary">
+        <div className="container-fluid">
             <Link className="navbar-brand" href="/">Toasty</Link>
-            <div className="navbar-nav me-auto">
-                {Object.entries(routeNames).map(([routeSegment, label]) => (
-                    <Link
-                        href={`/${routeSegment}`}
-                        key={routeSegment}
-                        className={routeSegment == segment ? "nav-link active" : "nav-link"}
-                        aria-current={routeSegment == segment ? "page" : undefined}
-                    >{label}</Link>
-                ))}
-            </div>
-            {user != null ? (
-                <div className="nav-item dropdown">
-                    <UserLink user={user} className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" showLocked />
-                    <ul className="dropdown-menu dropdown-menu-end">
-                        <li><Link className="dropdown-item" href={`/users/${user.networkId}`}>Profile</Link></li>
-                        <li><Link className="dropdown-item text-danger" href="/auth/logout">Log out</Link></li>
-                    </ul>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarToggler">
+                <div className="navbar-nav me-auto">
+                    {Object.entries(routeNames).map(([routeSegment, label]) => (
+                        <Link
+                            href={`/${routeSegment}`}
+                            key={routeSegment}
+                            className={routeSegment == segment ? "nav-link active" : "nav-link"}
+                            aria-current={routeSegment == segment ? "page" : undefined}
+                        >{label}</Link>
+                    ))}
                 </div>
-            ) : <LoadingLink variant="primary" href="/auth/login">Log in</LoadingLink>}
+                {user != null ? (
+                    <div className="navbar-nav dropdown">
+                        <UserLink user={user} className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" showLocked />
+                        <ul className="dropdown-menu dropdown-menu-end">
+                            <li><Link className="dropdown-item" href={`/users/${user.networkId}`}>Profile</Link></li>
+                            <li><Link className="dropdown-item text-danger" href="/auth/logout">Log out</Link></li>
+                        </ul>
+                    </div>
+                ) : <LoadingLink variant="primary" href="/auth/login">Log in</LoadingLink>}
+            </div>
         </div>
     </nav>;
 }
